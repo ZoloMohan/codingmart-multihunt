836793602bf261ff7f56a41dd8811263244b76b4
ZoloMohan <mohanzolomon@gmail.com>
2019-07-14 15:21:00 +0530

    Improved Collision Accuracy Check, Refactored Code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 var area = $('.arrow');
 var arrow = $('#arrowObj');
 var bow = $('#bow');
-var bubble_0 = $('.bubble');
-var bubble_1 = $('.bubble1');
-var bubble_2 = $('.bubble2');
-var currentBubbleX = false;
-var currentBubble1X = false;
-var currentBubble2X = false;
+var bird0 = $('.bubble');
+var bird1 = $('.bubble1');
+var bird2 = $('.bubble2');
+var hitBird0 = false;
+var hitBird1 = false;
+var hitBird2 = false;
 var currentArrowX = 0;
-var xOrigin = 0;
-var yOrigin = 0;
 var tries = 0;
 var hit = 0;
 var selector;
@@ -17,12 +15,11 @@ var selector;
 $(document).ready(function() {
     setInterval(createBrid, 2000);
     area.bind('mousemove',bowArrowMove);
-
-    area.click(function() {
+    area.click(function(event) {
         area.unbind("mousemove");
         checkHit();
         arrow.animate({ "left": "1000px" }, 2000, function(){
-            arrow.css({ "left": xOrigin, "top": yOrigin });
+            arrow.css({ "left": event.clientX, "top": event.clientY });
             area.bind('mousemove',bowArrowMove);
         });
     });    
@@ -32,19 +29,14 @@ function checkBubble0(){
     setTimeout(function(){
         let posLeft = parseInt(arrow.css('left'));
         if(posLeft > 300 && posLeft < 500){
-            console.log("Arrow Left: "+posLeft);
-            let bubble_0Top = parseInt(bubble_0.css('top'));
-            console.log("Bubble 0 Top: "+bubble_0Top);
+            let bubble_0Top = parseInt(bird0.css('top')) - 40;
             let arrowTop = parseInt(arrow.css('top'));
-            console.log("Arrow Top: "+arrowTop);
             if(Math.abs(arrowTop - bubble_0Top) < 40){
-                bubble_0.remove().fadeOut(function(){
-                    hit++;
-                    currentBubbleX=true;
+                bird0.remove().fadeOut(function(){
+                    hitBird0=true;
                     checkWin();
                 });
             }
-            console.log("HIT: "+hit);
         }
     },800);
 }
@@ -52,20 +44,15 @@ function checkBubble0(){
 function checkBubble1(){
     setTimeout(function(){
         let posLeft = parseInt(arrow.css('left'));
-        console.log("Arrow Left: "+posLeft);
         if(posLeft > 500 && posLeft < 700){
-            let bubble_1Top = parseInt(bubble_1.css('top'));
-            console.log("Bubble 1 Top: "+bubble_1Top);
-            let arrowTop = parseInt(arrow.css('top'));
-            console.log("Arrow Top: "+arrowTop);
+            let bubble_1Top = parseInt(bird1.css('top'));
+            let arrowTop = parseInt(arrow.css('top')) + 40;
             if(Math.abs(arrowTop - bubble_1Top) < 40){
-                bubble_1.remove().fadeOut(function(){
-                    hit++;
-                    currentBubble1X = true;
+                bird1.remove().fadeOut(function(){
+                    hitBird1 = true;
                     checkWin();
                 });
             }
-            console.log("HIT: "+hit);
         }
     },1200);
 }
@@ -73,20 +60,15 @@ function checkBubble1(){
 function checkBubble2(){
     setTimeout(function(){
         let posLeft = parseInt(arrow.css('left'));
-        console.log("Arrow Left: "+posLeft);
         if(posLeft > 700 && posLeft < 950){
-            let bubble_2Top = parseInt(bubble_2.css('top'));
-            console.log("Bubble 2 Top: "+bubble_2Top);
+            let bubble_2Top = parseInt(bird2.css('top')) - 40;
             let arrowTop = parseInt(arrow.css('top'));
-            console.log("Arrow Top: "+arrowTop);
             if(Math.abs(arrowTop - bubble_2Top) < 40){
-                bubble_2.remove().fadeOut(function(){
-                    hit++;
-                    currentBubble2X = true;
+                bird2.remove().fadeOut(function(){
+                    hitBird2 = true;
                     checkWin();
                 });
             }
-            console.log("HIT: "+hit);
         }
     },1600)
 }
@@ -94,13 +76,13 @@ function checkBubble2(){
 function checkHit(){
     // Your Code goes here
     // Both Bird and Arrow are moving objects.
-    if(!currentBubbleX) checkBubble0();
-    if(!currentBubble1X) checkBubble1();
-    if(!currentBubble2X) checkBubble2(); 
+    if(!hitBird0) checkBubble0();
+    if(!hitBird1) checkBubble1();
+    if(!hitBird2) checkBubble2(); 
 }
 
 function checkWin(){
-    if(currentBubble1X && currentBubble1X && currentBubble2X) alert("You Win, I Am Calling Blue Cross"); 
+    if(hitBird1 && hitBird1 && hitBird2) alert("You Win, I Am Calling Blue Cross"); 
 }
 
 function bowArrowMove(event){
@@ -111,7 +93,7 @@ function bowArrowMove(event){
 }
 
 function createBrid() {
-    selector = [bubble_0, bubble_1, bubble_2];
+    selector = [bird0, bird1, bird2];
     for (var i = 0; i < 3; i++) {
         var p = [400, 600, 800];
         selector[i].css({
